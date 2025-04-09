c9b0868371f9fb2edded89d882b87ab77e9ebce1
ManikandanGnanaGurusamy <manikandan.gnana@hcltech.com>
2025-04-09 14:50:56 +0530

    remove comented lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
+import "./App.css";
+import Login from "./pages/Auth/Login";
+import Home from "./pages/Dashboard/Home";
 
-import './App.css'
-import Login from './pages/Auth/Login';
-import Home from "./pages/Dashboard/Home"
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
- 
-
   return (
     <>
-   {/* <Login/> */}
-   <Router>
-      <Routes>
-        {/* Route for the login form */}
-        <Route path="/login" element={<Login />} />
-
-        {/* Route for the home page (only accessible after login) */}
-        <Route path="/home" element={<Home />} />
-        {/* <Route path="/borrow" element={<BorrowBook />}/>
-        <Route path="/return" element={<ReturnBook />}/> */}
-        {/* Optional: Redirect the root path to the login page */}
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
